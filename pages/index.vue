d3ceade65002cbb9a1bdb8ccd0a4c7db4979e627
Kyle Huffling <huff257@gmail.com>
2021-05-02 19:31:50 -0400

    Creating Home Page Sections

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,60 @@
 <template>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Kyle Huffling - Portfolio</title>
-  <meta name="description" content="Kyle Huffling Full Stack Web Developer">
-  <meta name="author" content="Kyle Huffling - Nuxt">
-</head>
-<body>
-
-  <h1 class="primary">KYLE HUFFLING <br>
-  WEB DEVELOPER</h1>
-
-
-  <script src="js/scripts.js"></script>
-</body>
-</html>
+<div class="page">
+<div class="container">
+  <div class="row">
+  <div class="col-55">
+  <div class="middle-align">
+  <h1 class="primary blue bottom-border-black spc-border-bottom">
+    KYLE HUFFLING <br>
+    WEB DEVELOPER</h1>
+  <p class="black spc-border-top">Full stack developer with over 5 years of professional experience. 
+    My focus is on building websites that work for people and businesses. </p>
+  </div>
+  </div>
+    <div class="col-45" style="display: flex;">
+      <img style="margin-left: auto;" src='~/assets/images/kylehuffling.png'>
+    </div>
+  </div>
+</div>
+<div class="blue-bg box-shadow margin-top">
+  <div class="container padding-50">
+  <!--<div class="row">
+  <div class="col-100">
+  <div class="">
+  <h2 class="primary white bottom-border-white spc-border-bottom">
+    MY SKILLS <br>
+    AND EXPERIENCE</h2>
+  </div>
+  </div>
+  </div>-->
+  <div class="row row-spc-bottom">
+  <div class="col-100">
+  <div class="">
+  <h2 class="primary white bottom-border-white spc-border-bottom">
+    EXAMPLES<br>
+    OF MY WORK</h2>
+  </div>
+  </div>
+  </div>
+  <div class="row row-spc">
+  <div class="col-100">
+  <div class="card-wrapper">
+    <div class="card">
+    </div>
+    <div class="card">
+    </div>
+    <div class="card">
+    </div>
+    <div class="card">
+    </div>
+    <div class="card">
+    </div>
+    <div class="card">
+    </div>
+  </div>
+  </div>
+  </div>
+  </div>
+</div>
+</div>
 </template>
