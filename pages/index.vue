3644858fb4e37d1bd290c17877c07620466a8af2
Kyle Huffling <huff257@gmail.com>
2021-05-02 17:18:31 -0400

    Style Changes

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,9 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-
   <title>Kyle Huffling - Portfolio</title>
   <meta name="description" content="Kyle Huffling Full Stack Web Developer">
   <meta name="author" content="Kyle Huffling - Nuxt">
-
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400;500;600&display=swap" rel="stylesheet">
-
 </head>
 <body>
 
