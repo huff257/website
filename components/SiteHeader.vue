fec3e0c17740bee33139029da6e8359807f58814
Kyle Huffling <huff257@gmail.com>
2021-05-07 21:28:00 -0400

    Adding Desktop Menu

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -2,11 +2,11 @@
 <nav class="main-nav black">
 <div class="row">
 <div class="site-logo">
-    <a href="/resume">KH</a>
+    <a href="/">KH</a>
 </div>
 <ul class="main-menu">
-    <li><a href="/resume">Resume</a></li>
-    <li><a href="/portfolio">Portfolio</a></li>
+    <li><a href="/">Resume</a></li>
+    <li><a href="/">Portfolio</a></li>
 </ul>
 </div>
 </nav>
