56a4df219fd72a8b976f9043ec459906c332dadd
Kyle Huffling <huff257@gmail.com>
2021-06-21 18:14:20 -0400

    typewritereffect page

--- pages/typewritereffect.vue
+++ pages/typewritereffect.vue
@@ -3,62 +3,7 @@
 <div class="container">
 <SiteHeader></SiteHeader>
 </div>
-<div class="container">
-  <div class="row">
-  <div class="col-55">
-  <div class="middle-align">
-  <h1 class="primary blue bottom-border-black spc-border-bottom">
-    KYLE HUFFLING <br>
-    WEB DEVELOPER</h1>
-  <p class="black spc-border-top">I am a full stack developer with over 5 years of professional experience. 
-    I build websites that work for people and businesses. </p>
-  </div>
-  </div>
-    <div class="col-45" style="display: flex;">
-      <img style="margin-left: auto;" src='~/assets/images/kylehuffling.png'>
-    </div>
-  </div>
-</div>
-<div class="blue-bg box-shadow margin-top">
-  <div class="container padding-50">
-  <!--<div class="row">
-  <div class="col-100">
-  <div class="">
-  <h2 class="primary white bottom-border-white spc-border-bottom">
-    MY SKILLS <br>
-    AND EXPERIENCE</h2>
-  </div>
-  </div>
-  </div>-->
-  <div class="row row-spc-bottom">
-  <div class="col-100">
-  <div class="">
-  <h2 class="primary white bottom-border-white spc-border-bottom">
-    EXAMPLES<br>
-    OF MY WORK</h2>
-  </div>
-  </div>
-  </div>
-  <div class="row row-spc">
-  <div class="col-100">
-  <div class="card-wrapper">
-    <div class="card">
-    </div>
-    <div class="card">
-    </div>
-    <div class="card">
-    </div>
-    <div class="card">
-    </div>
-    <div class="card">
-    </div>
-    <div class="card">
-    </div>
-  </div>
-  </div>
-  </div>
-  </div>
-</div>
+
 
 
 <!--Word Processor Effect-->
